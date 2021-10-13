64d67614534f0e3d440d2321ec6052e22cc8662e
sysmesh <sysmesh@users.noreply.github.com>
2021-10-13 17:25:40 +0300

    Update date_picker_i18n.dart
    US English + Hebrew Only

--- lib/src/i18n/date_picker_i18n.dart
+++ lib/src/i18n/date_picker_i18n.dart
@@ -1,29 +1,6 @@
 import 'dart:math';
 
 part 'strings_en_us.dart';
-part 'strings_zh_cn.dart';
-part 'strings_pt_br.dart';
-part 'strings_id.dart';
-part 'strings_ar_eg.dart';
-part 'strings_es.dart';
-part 'strings_ro.dart';
-part 'strings_bn.dart';
-part 'strings_bs.dart';
-part 'strings_ar.dart';
-part 'strings_jp.dart';
-part 'strings_ru.dart';
-part 'strings_de.dart';
-part 'strings_ko.dart';
-part 'strings_it.dart';
-part 'strings_hu.dart';
-part 'strings_hr.dart';
-part 'strings_uk.dart';
-part 'strings_tr.dart';
-part 'strings_vi.dart';
-part 'strings_fr.dart';
-part 'strings_sr_cyr.dart';
-part 'strings_sr_lat.dart';
-part 'strings_nl.dart';
 part 'strings_he.dart';
 
 abstract class _StringsI18n {
@@ -52,75 +29,6 @@ enum DateTimePickerLocale {
   /// English (EN) United States
   en_us,
 
-  /// Chinese (ZH) Simplified
-  zh_cn,
-
-  /// Portuguese (PT) Brazil
-  pt_br,
-
-  /// Indonesia (ID)
-  id,
-
-  /// Spanish (ES)
-  es,
-
-  /// Turkish (TR)
-  tr,
-
-  /// French (FR)
-  fr,
-
-  /// Romanian (RO)
-  ro,
-
-  /// Bengali (BN)
-  bn,
-
-  /// Bosnian (BS)
-  bs,
-
-  /// Arabic (ar)
-  ar,
-
-  /// Arabic (ar) Egypt
-  ar_eg,
-
-  /// Japanese (JP)
-  jp,
-
-  /// Russian (RU)
-  ru,
-
-  /// German (DE)
-  de,
-
-  /// Korea (KO)
-  ko,
-
-  /// Italian (IT)
-  it,
-
-  /// Hungarian (HU)
-  hu,
-
-  /// Croatian (HR)
-  hr,
-
-  /// Ukrainian (UK)
-  uk,
-
-  /// Vietnamese (VN)
-  vi,
-
-  /// Serbia (sr) Cyrillic
-  sr_cyrl,
-
-  /// Serbia (sr) Latin
-  sr_latn,
-
-  /// Dutch (NL)
-  nl,
-  
   /// Hebrew (HE)
   he,
 }
@@ -131,29 +39,6 @@ const DateTimePickerLocale DATETIME_PICKER_LOCALE_DEFAULT =
 
 const Map<DateTimePickerLocale, _StringsI18n> datePickerI18n = {
   DateTimePickerLocale.en_us: const _StringsEnUs(),
-  DateTimePickerLocale.zh_cn: const _StringsZhCn(),
-  DateTimePickerLocale.pt_br: const _StringsPtBr(),
-  DateTimePickerLocale.id: const _StringsId(),
-  DateTimePickerLocale.ar_eg: const _StringsArEg(),
-  DateTimePickerLocale.es: const _StringsEs(),
-  DateTimePickerLocale.fr: const _StringsFr(),
-  DateTimePickerLocale.ro: const _StringsRo(),
-  DateTimePickerLocale.bn: const _StringsBn(),
-  DateTimePickerLocale.bs: const _StringsBs(),
-  DateTimePickerLocale.ar: const _StringsAr(),
-  DateTimePickerLocale.jp: const _StringsJp(),
-  DateTimePickerLocale.ru: const _StringsRu(),
-  DateTimePickerLocale.de: const _StringsDe(),
-  DateTimePickerLocale.ko: const _StringsKo(),
-  DateTimePickerLocale.it: const _StringsIt(),
-  DateTimePickerLocale.hu: const _StringsHu(),
-  DateTimePickerLocale.hr: const _StringsHr(),
-  DateTimePickerLocale.uk: const _StringsUk(),
-  DateTimePickerLocale.tr: const _StringsTr(),
-  DateTimePickerLocale.vi: const _StringsVn(),
-  DateTimePickerLocale.sr_cyrl: const _StringsSrCyrillic(),
-  DateTimePickerLocale.sr_latn: const _StringsSrLatin(),
-  DateTimePickerLocale.nl: const _StringsNl(),
   DateTimePickerLocale.he: const _StringsHe(),
 };
 
